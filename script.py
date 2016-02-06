2e98026ff341b34b362c4dea68565c6ae55d42c7
backenddeveloper <grahamturnernew@hotmail.com>
2016-02-06 11:25:02 +0000

    Update script.py

--- script.py
+++ script.py
@@ -16,19 +16,19 @@ done
 def perform(command):
     os.system(command)
 
-#perform("mkdir temp_files")
-#perform("echo \"" + script + "\" > temp_files/test.sh")
-#perform("chmod +x temp_files/test.sh")
+perform("mkdir temp_files")
+perform("echo \"" + script + "\" > temp_files/test.sh")
+perform("chmod +x temp_files/test.sh")
 
 letters = ['A' , 'B' ,'C' , 'D' , 'E' , 'F']
 
-#for letter1 in letters:
-#    for letter2 in letters:
-#        for letter3 in letters:
-#            for letter4 in letters:
-#                for letter5 in letters:
-#                    for letter6 in letters:
-#                        perform("cd temp_files && wget http://s3-eu-west-1.amazonaws.com/puzzleinabucket/" + letter1 + letter2 + letter3 + letter4 + letter5 + letter6 + ".html &> /dev/null")
+for letter1 in letters:
+    for letter2 in letters:
+        for letter3 in letters:
+            for letter4 in letters:
+                for letter5 in letters:
+                    for letter6 in letters:
+                        perform("cd temp_files && wget http://s3-eu-west-1.amazonaws.com/puzzleinabucket/" + letter1 + letter2 + letter3 + letter4 + letter5 + letter6 + ".html &> /dev/null")
 
 perform("cd temp_files && ./test.sh > ../output.txt")
 perform("rm temp_files -rf")
